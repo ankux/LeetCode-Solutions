5cce2b1bf1c16ca232d42c255ef889c824bbe31f
Ankush Kumar <84901109+ankux@users.noreply.github.com>
2025-02-20 23:19:29 +0530

    Time: 73 ms (52.65%), Space: 14.4 MB (47.71%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -7,13 +7,12 @@ public:
 
         for(int i=0; i<n; i++){
             for(int j=0; j<i; j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = max(dp[i], dp[j]+1);
+                if(nums[j] < nums[i] && dp[i] < dp[j]+1){
+                    dp[i] = dp[j] + 1;
                 }
             }
             maxLIS = max(maxLIS, dp[i]);
         }
-        
         return maxLIS;
     }
 };
