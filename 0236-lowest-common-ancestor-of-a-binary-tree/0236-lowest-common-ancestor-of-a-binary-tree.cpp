2913c89cc9ce7b916dea3b7df6ed482eba2dade3
Ankush Kumar <84901109+ankux@users.noreply.github.com>
2025-05-16 07:35:58 +0530

    Time: 12 ms (64.1%), Space: 17.5 MB (40.92%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,26 +1,29 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == NULL || root == p || root == q)
+        if (!root) return NULL;
+
+        // If the current node is either p or q, we return the current node
+        // because one of the target nodes has been found
+        if(root == p || root == q) {
             return root;
-        
+        }
+
+        // Recur for the left and right subtrees
         TreeNode* left  = lowestCommonAncestor(root->left, p, q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
 
-        if (left == NULL)
-            return right;
-        else if (right == NULL)
-            return left; 
-        else
-            return root; //if left & right are not null, we found our res.
+        // If both left and right are non-NULL, it means p and q are found
+        // in different subtrees, so the current node is their LCA
+        if(left != NULL && right != NULL) {
+            return root;
+        } 
+
+        // If one of the left or right is non-NULL, return the non-NULL node
+        // which means either p or q is found in that subtree
+        if(left != NULL)
+            return left;
+        
+        return right;
     }
 };
